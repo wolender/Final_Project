f7ceeec5c41f64f03e7dc657b2e5778478d61a35
wolender <wolender@griddynamics.com>
2023-08-15 13:19:27 +0200

    initial commit

---
+++ terraform/jenkins.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+}
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "temp-web-server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = var.aws_key
+  subnet_id = aws_subnet.my_subnet1.id
+  security_groups = [aws_security_group.instances.id]
+  associate_public_ip_address  = true
+
+  user_data       = <<-EOF
+              #!/bin/bash
+              yum install httpd -y
+              service httpd enable
+              service httpd start
+              EOF
+  tags = {
+      Name = "temp-web-server"
+      Owner = var.owner
+      Project = var.project
+  }
+}
