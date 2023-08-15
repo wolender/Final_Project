abd0396ab403dc153b9e7e49c30982c3a59e41dd
wolender <wolender@griddynamics.com>
2023-08-15 13:19:27 +0200

    initial commit

---
+++ terraform/backend/variables.tf
@@ -0,0 +1,55 @@
+# General Variables
+
+variable "access_ip" {
+  description = "ip that can access the aplication"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+#must be part of region above
+variable "az1" {
+  description = "Avalabbiliity zone one for the application"
+  type        = string
+  default     = "eu-central-1a"
+}
+#must be part of region above
+variable "az2" {
+  description = "Availability zone two for the application"
+  type        = string
+  default     = "eu-central-1b"
+}
+
+#Tags
+variable "owner" {
+  description = "Owner tag"
+  type        = string
+  default     = "wolender"
+}
+
+variable "project" {
+  description = "Project name tag"
+  type        = string
+  default     = "2023_internship_warsaw"
+}
+# EC2 Variables
+
+variable "ami" {
+  description = "Amazon machine image to use for ec2 instance"
+  type        = string
+  default     = "ami-0dc7fe3dd38437495" # amazon machine eu-central
+}
+
+variable "aws_key" {
+  description = "Amazon machine image to use for ec2 instance"
+  type        = string
+  default     = "wolender_key" 
+}
+
+variable "instance_type" {
+  description = "ec2 instance type"
+  type        = string
+  default     = "t2.small"
+}
+
+
+
