9a539a3f82dbd83ef7dd2746e307108e6ba9ba14
wolender <wolender@griddynamics.com>
2023-08-18 13:48:37 +0200

    fix

--- terraform/Jenkinsfile.groovy
+++ terraform/Jenkinsfile.groovy
@@ -1,18 +1,9 @@
 pipeline {
     agent any
 
-    tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        maven "maven"
-    }
-    environment {
-        SETTINGS = '/var/lib/jenkins/settings.xml'
-    }
-
     stages {
         stage('Pull') {
             steps {
-                // Get some code from a GitHub repository
                 git branch: 'main', url:'https://github.com/wolender/Final_Project.git'
             }
         }
