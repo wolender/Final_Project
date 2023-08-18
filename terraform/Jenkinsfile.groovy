7bb38da30ea4c8e04c5062c93f9c9d1e810abbd3
wolender <wolender@griddynamics.com>
2023-08-18 15:03:24 +0200

    fix

--- terraform/Jenkinsfile.groovy
+++ terraform/Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
                 git branch: 'main', url:'https://github.com/wolender/Final_Project.git'
             }
         }
-    }
+    
         stage('Format') {
             steps{
                 dir('terraform') {
