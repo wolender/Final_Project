74e55c0a570672be759b4a4aa5afa3ccf354362b
wolender <wolender@griddynamics.com>
2023-08-21 13:16:14 +0200

    adding reconfigure

--- terraform/Jenkinsfile.groovy
+++ terraform/Jenkinsfile.groovy
@@ -21,7 +21,7 @@ pipeline {
         stage('Init') {
             steps{
                 dir('terraform') {
-                    sh 'terraform init'
+                    sh 'terraform init -migrate-state'
                 }
                
             }
