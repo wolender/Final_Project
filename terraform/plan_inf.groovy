b7db5fe99f3ee63196d989bb3a72251f46d720c9
wolender <wolender@griddynamics.com>
2023-08-21 13:42:21 +0200

    adding destroy and prov pipelines

---
+++ terraform/plan_inf.groovy
@@ -0,0 +1,48 @@
+pipeline {
+    agent any
+
+
+    stages {
+        stage('Pull') {
+            steps {
+                git branch: 'main', url:'https://github.com/wolender/Final_Project.git'
+            }
+        }
+
+        stage('Format') {
+            steps{
+                dir('terraform') {
+                    sh 'terraform fmt'
+                }
+               
+            }
+        }
+
+        stage('Init') {
+            steps{
+                dir('terraform') {
+                    sh 'terraform init -force-copy'
+                }
+               
+            }
+        }
+
+        stage('Validate') {
+            steps{
+                dir('terraform') {
+                    sh 'terraform validate'
+                }
+               
+            }
+        }
+
+        stage('Plan') {
+            steps{
+                dir('terraform') {
+                        sh 'terraform plan'
+                    }                    
+                }        
+        }
+
+    }
+}
