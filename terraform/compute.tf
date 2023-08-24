b694bb76f00af0d043a2ee0443e461e3bec53fac
wolender <wolender@griddynamics.com>
2023-08-24 10:18:48 +0200

    adding aws role

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "webserver1" {
   subnet_id = aws_subnet.my_subnet1.id
   security_groups = [aws_security_group.instances.id]
   associate_public_ip_address  = true
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   user_data       = <<-EOF
               #!/bin/bash
@@ -15,6 +16,16 @@ resource "aws_instance" "webserver1" {
               EOF
   tags = {
       Name = "webserver1"
+      Owner = var.owner
+      Project = var.project
+  }
+}
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "example-instance-profile"
+
+  roles = ["allow_ec2_ecr"]
+  tags = {
+      Name = "wolender_instance_pofile"
       Owner = var.owner
       Project = var.project
   }
