dea8406dd1cc5a21edd12988cacde90b22c97f9c
wolender <wolender@griddynamics.com>
2023-08-29 11:44:27 +0200

    skip snapszhoot

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "app-database" {
   parameter_group_name   = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.DB_sec_group.id]
   db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  skip_final_snapshot    = true
 
   tags = {
     Name    = "wolender-database"
